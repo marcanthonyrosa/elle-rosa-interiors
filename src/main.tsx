b8b89558d4c80616f7256a9dce055093683af39f
Marc Rosa <marcrosa@Marcs-MacBook-Air.local>
2026-02-23 10:16:38 -0600

    Caught up with package changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import posthog from 'posthog-js'
+
+posthog.init('phc_BHIQDrEpBMTu5QQ6fD6mhPVlJ9gVJ79jEXxjcFSck8T', {
+  api_host: 'https://us.i.posthog.com',
+})
+
 createRoot(document.getElementById("root")!).render(<App />);
+
