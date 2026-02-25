eb325ee46f11f749faf0778124418cfbb0a3aae8
Marc Rosa <marcrosa@Marcs-MacBook-Air.local>
2026-02-24 23:09:55 -0600

    Move Posthog key to env variable

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 
 import posthog from 'posthog-js'
 
-posthog.init('phc_BHIQDrEpBMTu5QQ6fD6mhPVlJ9gVJ79jEXxjcFSck8T', {
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
   api_host: 'https://us.i.posthog.com',
 })
 
